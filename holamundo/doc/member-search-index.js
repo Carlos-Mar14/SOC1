d30ef4d0450a44f9f1f2976d98b61769d77ca06c
FOAP <FOAP@iMac-de-Supervisor-3.local>
2024-02-02 19:49:52 +0100

    Ejercicios1&2

---
+++ holamundo/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"holamundo","c":"SaludoMatutino","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"holamundo","c":"SaludoMatutino","l":"SaludoMatutino()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ holamundo/src/holamundo/SaludoMatutino.java
@@ -0,0 +1,12 @@
+package holamundo;
+
+public class SaludoMatutino {
+
+	public static void main(String[] args) {
+		String nombre= "Carlos";
+		System.out.println("Hola Viernes");
+		System.out.println("Hola Sabado");
+		System.out.println("Hola Domingo");
+		System.out.println("Hola Viernes"+ nombre);
+	}
+}
